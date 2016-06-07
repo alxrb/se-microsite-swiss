8f561ec9dcf41f319f41d8bac582509704fb126f
Alex Robertson-Brown <alexrb@Alexs-MacBook-Pro-2.local>
2016-06-07 08:05:44 +0100

    full tested

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('htmlMinify', function() {
 gulp.task('optimiseCss', function() {
   return gulp.src('./_site/**/*.css')
   .pipe(autoprefix({
-    browsers: ['last 2 versions'],
+    browsers: ['last 3 versions'],
     cascade: false
   }))
   .pipe(cssmin())
